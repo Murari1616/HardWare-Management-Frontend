a6b4fb67246ee57006e53f4a42c409c5a793f5ba
Murari1616 <murarikunchapu@gmail.com>
2025-06-29 19:57:08 +0530

    new problem

--- src/appConstants.js
+++ src/appConstants.js
@@ -1,7 +1,7 @@
 //@ts-nocheck
-export const BASE_URL = import.meta.env.PUBLIC_BACKEND_URL||"http://localhost:4000/api/v1/";
-export const socketURL = import.meta.env.PUBLIC_SOCKET_URL||"http://localhost:4000";
-export const upiId=import.meta.env.UPI_ID||"123456789@upi";
-export const UpiName=import.meta.env.UPI_NAME||"FIRST SECOND";
-export const phoneNumber=import.meta.env.UPI_NUMBER||1234567890;
-export const publicKey=import.meta.env.PUBLIC_KEY||'BAUkvzPVmTbbF-3cmyyo4HNnu7kmFXGstq5OLRtDdZCfgESaAsWh7GIiuu--em4LEbIsJLc9E2wFlqem93JPsiY'
+export const BASE_URL = import.meta.env.VITE_PUBLIC_BACKEND_URL || "http://localhost:4000/api/v1/";
+export const socketURL = import.meta.env.VITE_PUBLIC_SOCKET_URL || "http://localhost:4000";
+export const upiId = import.meta.env.VITE_UPI_ID || "123456789@upi";
+export const UpiName = import.meta.env.VITE_UPI_NAME || "FIRST SECOND";
+export const phoneNumber = import.meta.env.VITE_UPI_NUMBER || 1234567890;
+export const publicKey = import.meta.env.VITE_PUBLIC_KEY || 'BAUkvzPVmTbbF-3cmyyo4HNnu7kmFXGstq5OLRtDdZCfgESaAsWh7GIiuu--em4LEbIsJLc9E2wFlqem93JPsiY';
