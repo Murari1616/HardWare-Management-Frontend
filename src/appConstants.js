2b83ce05cc4e3c4713f200fbb12c0b34cd8daf82
Murari1616 <murarikunchapu@gmail.com>
2025-06-29 19:49:12 +0530

    problem with env

--- src/appConstants.js
+++ src/appConstants.js
@@ -1,6 +1,7 @@
-export const BASE_URL = process.env.PUBLIC_BACKEND_URL||"http://localhost:4000/api/v1/";
-export const socketURL = process.env.PUBLIC_SOCKET_URL||"http://localhost:4000";
-export const upiId=process.env.UPI_ID||"123456789@upi";
-export const UpiName=process.env.UPI_NAME||"FIRST SECOND";
-export const phoneNumber=process.env.UPI_NUMBER||1234567890;
-export const publicKey=process.env.PUBLIC_KEY||'BAUkvzPVmTbbF-3cmyyo4HNnu7kmFXGstq5OLRtDdZCfgESaAsWh7GIiuu--em4LEbIsJLc9E2wFlqem93JPsiY'
+//@ts-nocheck
+export const BASE_URL = import.meta.env.PUBLIC_BACKEND_URL||"http://localhost:4000/api/v1/";
+export const socketURL = import.meta.env.PUBLIC_SOCKET_URL||"http://localhost:4000";
+export const upiId=import.meta.env.UPI_ID||"123456789@upi";
+export const UpiName=import.meta.env.UPI_NAME||"FIRST SECOND";
+export const phoneNumber=import.meta.env.UPI_NUMBER||1234567890;
+export const publicKey=import.meta.env.PUBLIC_KEY||'BAUkvzPVmTbbF-3cmyyo4HNnu7kmFXGstq5OLRtDdZCfgESaAsWh7GIiuu--em4LEbIsJLc9E2wFlqem93JPsiY'
